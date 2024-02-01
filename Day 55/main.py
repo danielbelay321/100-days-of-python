96c3ab35880cc2f47f8bda00c972aecbaa53586f
danielbelay321 <76012888+danielbelay321@users.noreply.github.com>
2024-02-01 16:23:29 +0300

    Update main.py

--- Day 55/main.py
+++ Day 55/main.py
@@ -1,1 +1,34 @@
+from flask import Flask
+from markupsafe import escape
+from random import choice, randint
+
+
+
+app = Flask(__name__)
+
+random_number = randint(0,9)
+
+@app.route('/<int:number>')
+def hello(number):
+
+    if number < random_number:
+        return "<h1 style='color:red;'>Too Low, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif' alt='number guess'>"
+    elif number > random_number:
+        return "<h1 style='color:red;'>Too High, try again!</h1>" \
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif' alt='number guess'>"
+    else:
+        return "<h1 style='color:green;'>Congratulation!, Yo got it!</h1>" \
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif' alt='number guess'>"
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
 
