a6bdf94a6c78b71ec0eb54ff07901ed6ade2c8f3
danielbelay321 <76012888+danielbelay321@users.noreply.github.com>
2024-02-03 20:01:41 +0300

    Blog Capstone Project Part 1 - Templating

--- Day 57 Blog Capstone Project Part 1 - Templating/main.py
+++ Day 57 Blog Capstone Project Part 1 - Templating/main.py
@@ -1,1 +1,26 @@
+from flask import Flask, render_template
+import requests
 
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    urls = 'https://api.npoint.io/30e19a1dcb30815b1025'
+    response_link = requests.get(urls)
+    response_link.raise_for_status()
+
+    posts = response_link.json()
+    return render_template("index.html", posts=posts)
+
+@app.route('/post/<int:id>')
+def post(id):
+    urls = 'https://api.npoint.io/30e19a1dcb30815b1025'
+    response_link = requests.get(urls)
+    response_link.raise_for_status()
+
+    posts = response_link.json()
+    return render_template("post.html", posts=posts, id=id)
+
+if __name__ == "__main__":
+    app.run(debug=True)
